4e6d0b7a74f1ed26fdb9e744e5b5dc19745a0e38
Seok Kwun Park <park960@purdue.edu>
2018-06-17 17:43:22 -0400

    update tableviewcell when user enters an item

--- undisclosed/ViewController.swift
+++ undisclosed/ViewController.swift
@@ -17,17 +17,13 @@ class TableViewCell: UITableViewCell {
         // Initialization code
     }
     
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-        
-        // Configure the view for the selected state
-    }
-    
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    var items:[Items]!
+    var items = ["sdfkldsf", "owiejfoijw"]
+    
+    @IBOutlet weak var tableView: UITableView!
     
     @IBAction func addItem(_ sender: Any) {
         //create alert controller
@@ -40,11 +36,16 @@ class ViewController: UIViewController {
         alert.addTextField{
             (textfield: UITextField) in textfield.placeholder = "item"
         }
+        
         //add create btn
         alert.addAction(UIAlertAction(
             title: "Create",
             style: .default,
-            handler: {(action:UIAlertAction) in}
+            handler: {(action:UIAlertAction) in
+                guard let item = alert.textFields?.first?.text else {return}
+                self.items.append(item)
+                self.tableView.reloadData()
+            }
         ))
         //add cancel btn
         alert.addAction(UIAlertAction(
@@ -54,17 +55,28 @@ class ViewController: UIViewController {
         ))
         
         self.present(alert, animated: true, completion: nil)
-    }
+    }//
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-    }
+        tableView.delegate = self
+        tableView.dataSource = self
+    }//
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }//
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return items.count
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+        cell.listLabel.text = items[indexPath.row]
+        return cell
     }
 
 
