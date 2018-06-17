df8822e35f0bb9e930eb082f547195741ab34680
Seok Kwun Park <park960@purdue.edu>
2018-06-16 23:07:09 -0400

    delete TableView.Cell.swift, add alert/inputbox for user to enter items

--- undisclosed/ViewController.swift
+++ undisclosed/ViewController.swift
@@ -8,8 +8,54 @@
 
 import UIKit
 
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var listLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
+
 class ViewController: UIViewController {
 
+    var items:[Items]!
+    
+    @IBAction func addItem(_ sender: Any) {
+        //create alert controller
+        let alert = UIAlertController(
+            title: "New Item",
+            message: "Enter an item",
+            preferredStyle: .alert
+        )
+        //add textfield
+        alert.addTextField{
+            (textfield: UITextField) in textfield.placeholder = "item"
+        }
+        //add create btn
+        alert.addAction(UIAlertAction(
+            title: "Create",
+            style: .default,
+            handler: {(action:UIAlertAction) in}
+        ))
+        //add cancel btn
+        alert.addAction(UIAlertAction(
+            title: "Cancel",
+            style: .cancel,
+            handler: nil
+        ))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
