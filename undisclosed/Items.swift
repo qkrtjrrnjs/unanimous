beef310d8d4a6d5244193709957ab068b9a5f612
Seok Kwun Park <park960@purdue.edu>
2018-06-16 23:06:18 -0400

    create struct

---
+++ undisclosed/Items.swift
@@ -0,0 +1,13 @@
+//
+//  Items.swift
+//  undisclosed
+//
+//  Created by chris on 6/16/18.
+//  Copyright © 2018 YSYP. All rights reserved.
+//
+
+import Foundation
+
+struct Items {
+    var name:String
+}
